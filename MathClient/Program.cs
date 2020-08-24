e2b6af8f44295629c82a9adea865d768fa5fbbb7
Stefan Gherman <scgheman@gmail.com>
2020-08-24 09:57:43 +0300

    Add custom binding

--- MathClient/Program.cs
+++ MathClient/Program.cs
@@ -2,6 +2,7 @@
 using System.Collections.Generic;
 using System.Linq;
 using System.ServiceModel;
+using System.ServiceModel.Channels;
 using System.Text;
 using System.Threading.Tasks;
 
@@ -16,9 +17,17 @@ namespace MathClient
             mreq.request = req;
             mreq.CustomHeader = 8;
             MathTypes.MathResponseMessage response;
+            //NetTcpBinding binding = new NetTcpBinding();
+            //binding.Security.Mode = SecurityMode.Message;
+            //binding.Security.Message.ClientCredentialType = MessageCredentialType.Windows;
+            //binding.Security.Transport.ProtectionLevel =
+            //System.Net.Security.ProtectionLevel.EncryptAndSign;
+            CustomBinding binding = new CustomBinding();
+            binding.Elements.Add(new TextMessageEncodingBindingElement());
+            binding.Elements.Add(new TcpTransportBindingElement());
             MathTypes.IMath channel = new ChannelFactory<MathTypes.IMath>(
-                new BasicHttpBinding(),
-                new EndpointAddress("http://localhost:8080/math")).CreateChannel();
+                binding,
+                new EndpointAddress("net.tcp://localhost:8081/math")).CreateChannel();
             response = channel.Add(mreq);
             Console.WriteLine("Call via BasicProfileBinding: {0}",
             response.response.result);
--- MathServiceHost/Program.cs
+++ MathServiceHost/Program.cs
@@ -3,6 +3,7 @@ using System.Collections.Generic;
 using System.Configuration;
 using System.Linq;
 using System.ServiceModel;
+using System.ServiceModel.Channels;
 using System.ServiceModel.Description;
 using System.Text;
 using System.Threading.Tasks;
@@ -14,12 +15,28 @@ namespace MathServiceHost
         private static void Main(string[] args)
         {
             ServiceHost host = new ServiceHost(
-typeof(MathService),
-new Uri(ConfigurationManager.AppSettings["baseAddress"]));
+            typeof(MathService),
+            new Uri(ConfigurationManager.AppSettings["baseAddress"]),
+            new Uri(ConfigurationManager.AppSettings["tcpBaseAddress"]));
             host.AddServiceEndpoint(
             typeof(MathTypes.IMath),
             new BasicHttpBinding(),
             "math");
+            //NetTcpBinding binding = new NetTcpBinding();
+            //binding.Security.Mode = SecurityMode.Message;
+            //binding.Security.Message.ClientCredentialType = MessageCredentialType.Windows;
+            //binding.Security.Transport.ProtectionLevel =
+            //System.Net.Security.ProtectionLevel.EncryptAndSign;
+            CustomBinding binding = new CustomBinding();
+            binding.Elements.Add(new TextMessageEncodingBindingElement());
+            binding.Elements.Add(new TcpTransportBindingElement());
+            host.AddServiceEndpoint(
+            typeof(MathTypes.IMath),
+            binding,
+            "math");
+            ServiceMetadataBehavior meta = new ServiceMetadataBehavior();
+            meta.HttpGetEnabled = true;
+            host.Description.Behaviors.Add(meta);
             host.Open();
             Console.WriteLine(
             "MathService is listening on the following...");
